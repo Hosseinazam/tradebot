dacf706c94c41d5122baa013a4afe90d961add5a
Hosseinazam <hossein.zarghami1267@gmail.com>
2025-12-03 01:03:32 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,31 @@
 // server.js - very simple webhook receiver (Node.js + Express)
-const express = require('express');
+const express = require('express');const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = "mongodb+srv://tradebotuser:secret123@cluster0.pz0qrex.mongodb.net/tradebot?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+let signalsCollection;
+
+async function connectDB() {
+    try {
+        await client.connect();
+        const db = client.db("tradebot");
+        signalsCollection = db.collection("signals");
+        console.log("MongoDB connected ✅");
+    } catch (err) {
+        console.error("MongoDB connection error:", err);
+    }
+}
+
+connectDB();
+
 const fs = require('fs');
 const app = express();
 app.use(express.json());
